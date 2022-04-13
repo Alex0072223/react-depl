765a0dfaa91eb2723f0fe72f9be697854d6d02c0
alex <alex0072223@mail.ru>
2022-04-13 07:28:08 +0400

    Show must go on

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from "react";
+import Counter from "./Components/Counter";
+import ClassCounter from "./Components/ClassCounter";
+import PostItem from "./Components/PostItem";
+import "./Styles/App.css"
+import PostList from "./Components/PostList";
+import MyButton from "./Components/UI/button/MyButton";
+import MyInput from "./Components/UI/input/MyInput";
 
 function App() {
+
+    const [value, setValue] = useState("spalme text")
+    const [posts,setPosts] = useState([
+        {id: 1, title: "JSSSs 1", body: "some description"},
+        {id: 2, title: "JSSSS 2", body: "some description"},
+        {id: 3, title: "JSSSS 3", body: "some description"},
+    ])
+    const [posts2,setPosts2] = useState([
+        {id: 1, title: "No SS 3", body: "some description"},
+    ])
+
+
+
+
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+
+    const  addNewPost = (e) => {
+        e.preventDefault() /*Предотвращает обновление страницы после нажатия кнопки*/
+        const newPost = {
+            id: Date.now(),
+            title,
+            body
+        }
+        setPosts([...posts,newPost])
+        setTitle('')
+        setBody('')
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <form>
+            <MyInput
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                type="text"
+                placeholder="Name of Post"
+            />
+            {/*/!*<input ref={bodyInputRef} type="text"/>*!/ напрямую из DOM*/}
+            <MyInput
+                value={body}
+                onChange={e => setBody(e.target.value)}
+                type="text"
+                placeholder="Description of Post"
+            />
+            <MyButton onClick={addNewPost}>Create!</MyButton>
+        </form>
+
+        <PostList posts={posts} title={"список постов 1"}/>
+        <PostList posts={posts2} title={"some other of them"}/>
+
+        <Counter/>
+
+        <h1>{value}</h1>
+        <input
+            type="text"
+            value={value}
+            onChange={event => setValue(event.target.value)}
+        />
+
+        <ClassCounter/>
+
+
+
+
     </div>
   );
 }
